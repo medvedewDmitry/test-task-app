04d24add8b547cd18d3252b5a4a2bdf41436cd1e
Medvedew Dmitrij <medvedewdmitrij@yandex.ru>
2022-12-12 16:38:54 +0700

    edit project

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-import HeaderLogo from "../ComponentsUIKit/HeaderLogo/HeaderLogo";
 import MenuButton from "../ComponentsUIKit/MenuButton/MenuButton";
 
 const Header = () => {
   return (
     <header className="header col-lg-12 col-md-8 col-sm-4">
       <div className="header__heading-logo col-lg-11 col-md-8">
-        <HeaderLogo />
+     
         <p className="p_14 gray col-sm-2">
           <span>Медведев</span>
           <br />
